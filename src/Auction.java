0206682369a9db25642d4df831750a110a36d397
mmmm <84239148+TaVietAnh9102@users.noreply.github.com>
2022-10-28 11:50:52 +0700

    dtb_demo

--- src/Auction.java
+++ src/Auction.java
@@ -1,17 +1,18 @@
+import java.sql.*;
 
 public class Auction {
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("ta");
-		System.out.println("dob");
-		System.out.println("Hello");
-		System.out.println("ta");
-		System.out.println("dob");
-		System.out.println("Hello");
-		System.out.println("ta");
-		System.out.println("dob");
-		System.out.println("Hello");
-		System.out.println("ta");
-		System.out.println("dob");
+		Connection connection = null;
+	    String dbName = "auction";
+	    String dbURL = "jdbc:mysql://localhost:3306/" + dbName;
+	    String dbUsername = "root";
+	    String dbPassword = "2112";
+
+        try {
+            connection = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
+            System.out.print("Database Open Successfully");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 	}
 }
