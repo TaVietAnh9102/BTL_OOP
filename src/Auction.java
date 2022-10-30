7e50840dddd6544a6ff98962f0107a28e10d553c
Trần Minh Châu <dluffy2332@gmail.com>
2022-10-30 16:06:16 +0700

    2

--- src/Auction.java
+++ src/Auction.java
@@ -3,9 +3,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import models.DBInterface;
+import view.LoginView;
 
 public class Auction {
 	public static void main(String[] args) {
+//		DBInterface dbInterface = DBInterface.getInstance();
+//		Scanner scanner = new Scanner(System.in);
+//		String table = scanner.next();
+//		ArrayList<Object[]> list = dbInterface.select(table);
+//		for(Object[] user : list) {
+//           System.out.println(user[1]);
+//        }
+//		scanner.close();
+//		new LoginView();
 		DBInterface dbInterface = DBInterface.getInstance();
 		Scanner scanner = new Scanner(System.in);
 		String table = scanner.next();
@@ -14,5 +24,6 @@ public class Auction {
            System.out.println(user[1]);
         }
 		scanner.close();
+		new LoginView();
 	}
 }
